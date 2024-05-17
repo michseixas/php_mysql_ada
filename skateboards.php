06e6c67d287298cecbcae27a0a1df754fae19979
Mich Seixas <mcseixas@gmail.com>
2024-05-17 17:26:17 +0200

    added images, looping in images

--- skateboards.php
+++ skateboards.php
@@ -1,39 +1,67 @@
 <?php
-include_once("controller.php")
+include_once("controller.php");
+
+// Glob dynamically fetch all image files from the img directory and then 
+//displays them in an HTML table. Any new images added to the directory will automatically be included. 
+//It the creates an array to be easily iterated.
+//GLOB_BRACE flag allows the use of the {} syntax to match multiple patterns.
+$images = glob('img/*.{jpg,jpeg,png,gif}', GLOB_BRACE);
 ?>
 
+<!DOCTYPE html>
 <html>
+
+<head>
     <style>
-    table, th, td {
-        border: 1px solid black;
-        border-collapse: collapse;
-    }
+        table,
+        th,
+        td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+
+        th,
+        td {
+            padding: 10px;
+        }
     </style>
-    <body>
-        <p>
+</head>
+
+<body>
+    <p>
         <?php
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             echo 'Concierto agregado!';
         }
         ?>
-        </p>
+    </p>
 
-        <h1>Skate list</h1>
-        <table>
+    <h1>Skate list</h1>
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Brand</th>
+            <th>Price</th>
+            <th>Image</th>
+        </tr>
+        <!-- The foreach loop iterates over the $skates array, and for each skate, it checks if there is a corresponding image in the $images array. -->
+        <?php foreach ($skates as $key => $value) : ?>
             <tr>
-                <th>Name</th>
-                <th>Brand</th>
+                <td><?= htmlspecialchars($value['name']); ?></td>
+                <td><?= htmlspecialchars($value['brand']); ?></td>
+                <td><?= htmlspecialchars($value['price']); ?></td>
+                <td>
+                    <?php if (isset($images[$key])) : ?> <!-- checks the $images array & sees if there is an image corresponding to the current skate item and displays it -->
+                        <img src="<?= htmlspecialchars($images[$key]); ?>" alt="Skate Image" width="100" height="100" style="border:2px solid black">
+                    <?php else : ?>
+                        No image available
+                    <?php endif; ?>
+                </td>
             </tr>
-            <?php foreach ($skates as $clave => $valor): ?> 
-                <tr>
-                <td><?= $valor['name']; ?></td>
-                <td><?= $valor['brand']; ?></td>
-                </tr>
-            <?php endforeach; ?>
-        </table>
-        <br><br>
-        <!-- <a href="crear_concierto.php"> Crear Concierto >>></a> -->
-    </body>
-
+        <?php endforeach; ?>
+    </table>
+    <br><br>
+    <!-- <a href="crear_concierto.php"> Crear Concierto >>></a> -->
+</body>
 
 </html>
